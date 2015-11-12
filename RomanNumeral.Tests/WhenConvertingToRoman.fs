a512f9e794b3481b6a47c6b2dab70d4cad1e7907
Alessandro Di Gioia <Alessandro Di Gioia>
2015-11-12 16:47:21 +0000

    Renamed files to match REAL intent

---
+++ RomanNumeral.Tests/WhenConvertingToRoman.fs
@@ -0,0 +1,24 @@
+﻿module WhenConvertingToRoman
+
+    open NUnit.Framework
+    open FsCheck
+    open FsCheck.NUnit
+    open Swensen.Unquote
+
+    [<Test>]
+    //used to enable test discovery in NCrunch
+    let ignore_me () = ()
+
+
+//    Conversion Table
+//    1    - I
+//    5    - V
+//    10   - X
+//    50   - L
+//    100  - C
+//    500  - D
+//    1000 - M
+//    2499 - MMCDXCIX
+//    3949 - MMMCMXLIX
+
+    
