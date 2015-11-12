12dde783bb27ec8c8d4c9f43bb6a693cbb0d066f
Alessandro Di Gioia <Alessandro Di Gioia>
2015-11-12 16:34:05 +0000

    Project ready for TDD Kata

---
+++ RomanNumeral.Tests/WhenConvertingToArabic.fs
@@ -0,0 +1,10 @@
+﻿module WhenConvertingToArabic
+
+    open NUnit.Framework
+    open FsCheck
+    open FsCheck.NUnit
+    open Swensen.Unquote
+
+    [<Test>]
+    //used to enable test discovery in NCrunch
+    let ignore_me () = ()
