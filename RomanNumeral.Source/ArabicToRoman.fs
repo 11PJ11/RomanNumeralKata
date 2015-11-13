a8b1064ecaa7ce00e6f9ce0234efdca3620e3829
Alessandro Di Gioia <Alessandro Di Gioia>
2015-11-13 14:31:11 +0000

    moved remainder in the data aravbToRoman data structure

--- RomanNumeral.Source/ArabicToRoman.fs
+++ RomanNumeral.Source/ArabicToRoman.fs
@@ -1,13 +1,13 @@
 ﻿module ArabicToRoman
 
-    type ArabToRoman = { arab : int; roman : string; }
+    type ArabToRoman = { arab : int; roman : string; remainder : int}
 
     let mapArabToRoman = 
         function
-        | x when x >= 10 -> { arab = 10; roman = "X"}
-        | x when x >= 5 -> { arab = 5; roman = "V"}
-        | x when x >= 1 -> { arab = 1; roman = "I"}
-        | _ -> { arab = 0; roman = ""}
+        | x when x >= 10 -> { arab = 10; roman = "X"; remainder = x - 10}
+        | x when x >= 5 -> { arab = 5; roman = "V"; remainder = x - 5}
+        | x when x >= 1 -> { arab = 1; roman = "I"; remainder = x - 1}
+        | x -> { arab = 0; roman = ""; remainder = x}
 
 
 
